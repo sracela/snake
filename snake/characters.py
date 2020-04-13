a5c8a210afde8d31bf0a54157690bc0115124a0f
sracela <saracelalfonso@gmail.com>
2020-04-13 23:13:05 +0200

    Add enum classes

---
+++ snake/characters.py
@@ -0,0 +1,7 @@
+from enum import Enum, auto
+
+
+class Characters(Enum):
+    SNAKE_GOAL = auto()
+    HEAD = auto()
+    BODY = auto()
---
+++ snake/state.py
@@ -0,0 +1,8 @@
+from enum import Enum, auto
+
+
+class State(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
